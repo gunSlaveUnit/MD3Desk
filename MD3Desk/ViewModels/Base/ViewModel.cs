4212f8036526f8f3d8f37744e7ac62161c9898a5
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-10-19 12:58:42 +0300

    Added ViewModel class

---
+++ MD3Desk/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace MD3Desk.ViewModels.Base;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+        
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
