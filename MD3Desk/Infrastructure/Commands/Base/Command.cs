a38aa8918520791bbc6ddb19f4eab081871b7409
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-10-19 12:55:45 +0300

    Created command class

---
+++ MD3Desk/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows.Input;
+
+namespace MD3Desk.Infrastructure.Commands.Base;
+
+public abstract class Command : ICommand
+{
+    public abstract bool CanExecute(object parameter);
+
+    public abstract void Execute(object parameter);
+
+    public event EventHandler CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
