3c916407b9de03b87297e14522a5aab5683e5ba1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-10-26 08:32:54 +0300

    Added current view model property

--- MD3Desk/ViewModels/MainViewModel.cs
+++ MD3Desk/ViewModels/MainViewModel.cs
@@ -4,5 +4,15 @@ namespace MD3Desk.ViewModels;
 
 public class MainViewModel : ViewModel
 {
+    #region Properties 
     
+    private ViewModel _currentViewModel;
+    
+    public ViewModel CurrentViewModel
+    {
+        get => _currentViewModel;
+        private set => Set(ref _currentViewModel, value);
+    }
+    
+    #endregion
 }
