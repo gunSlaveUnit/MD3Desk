1cdd1f1c59fa7b074eab18df0fe576134f5d8d28
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-02 11:11:40 +0300

    SHOS view cs

---
+++ MD3Desk/Views/Displays/SHOS.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MD3Desk.Views.Displays;
+
+public partial class SHOS : UserControl
+{
+    public SHOS()
+    {
+        InitializeComponent();
+    }
+}
