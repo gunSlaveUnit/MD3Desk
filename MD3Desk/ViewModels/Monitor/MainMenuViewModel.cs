ed6d32f70ada7ec93e409b8825b92a7a9e7863ad
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-02 10:40:35 +0300

    Added navigation to shos command

--- MD3Desk/ViewModels/Monitor/MainMenuViewModel.cs
+++ MD3Desk/ViewModels/Monitor/MainMenuViewModel.cs
@@ -1,8 +1,36 @@
+using System.Windows.Input;
+using MD3Desk.Infrastructure.Commands.Base;
 using MD3Desk.ViewModels.Base;
 
 namespace MD3Desk.ViewModels.Monitor;
 
 public class MainMenuViewModel : ViewModel
 {
+    #region Properties
+
+    private readonly MainViewModel _parent;
+
+    #endregion
+
+    #region Commands
+
+    #region NavigateSHOSCommand
+        
+    private ICommand _navigateSHOSCommand;
+    public ICommand NavigateSHOSCommand => _navigateSHOSCommand
+        ??= new RelayCommand(OnNavigateSHOSCommandExecuted, CanNavigateSHOSCommandExecute);
+
+    private void OnNavigateSHOSCommandExecuted(object parameter)
+        => _parent.CurrentViewModel = _parent.NoSHOSWarningVm;
+        
+    private bool CanNavigateSHOSCommandExecute(object parameter) => true;
+        
+    #endregion
+
+    #endregion
     
+    public MainMenuViewModel(MainViewModel parent)
+    {
+        _parent = parent;
+    }
 }
