4cb40e4fa7c8bcbf78371e05c1b02d5539e3aad8
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-09 08:58:25 +0300

    Added command to navigate to settings

--- MD3Desk/ViewModels/Monitor/Settings/CommonViewModel.cs
+++ MD3Desk/ViewModels/Monitor/Settings/CommonViewModel.cs
@@ -1,3 +1,5 @@
+using System.Windows.Input;
+using MD3Desk.Infrastructure.Commands.Base;
 using MD3Desk.ViewModels.Base;
 
 namespace MD3Desk.ViewModels.Monitor.Settings;
@@ -5,6 +7,23 @@ namespace MD3Desk.ViewModels.Monitor.Settings;
 public class CommonViewModel : ViewModel
 {
     private readonly MainViewModel _parent;
+
+    #region Commands
+
+    #region NavigateMainMenuCommand
+        
+    private ICommand _navigateSettingsCommand;
+    public ICommand NavigateSettingsCommand => _navigateSettingsCommand
+        ??= new RelayCommand(OnNavigateSettingsCommandExecuted, CanNavigateSettingsCommandExecute);
+
+    private void OnNavigateSettingsCommandExecuted(object parameter)
+        => _parent.CurrentViewModel = _parent.SettingsVm;
+        
+    private bool CanNavigateSettingsCommandExecute(object parameter) => true;
+        
+    #endregion
+
+    #endregion
 
     public CommonViewModel(MainViewModel parent)
     {
