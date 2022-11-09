dc7e8c4c2c02a1af30d298ced61c4b73f2bdaf7b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-09 14:59:04 +0300

    Added Station emulator window property

--- MD3Desk/ViewModels/MainViewModel.cs
+++ MD3Desk/ViewModels/MainViewModel.cs
@@ -1,9 +1,26 @@
 using MD3Desk.ViewModels.Base;
+using MD3Desk.Views.Windows;
 
 namespace MD3Desk.ViewModels;
 
 public class MainViewModel : ViewModel
 {
+    #region Properties
+
+    #region StationEmulatorWindow 
+    
+    private StationEmulatorWindow _stationEmulatorWindow;
+    
+    public StationEmulatorWindow StationEmulatorWindow
+    {
+        get => _stationEmulatorWindow;
+        set => Set(ref _stationEmulatorWindow, value);
+    }
+    
+    #endregion
+
+    #endregion
+    
     public MainViewModel()
     {
         
