313a8641a4fa9718414406779a6b827243a2bbf9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-09 08:51:23 +0300

    Added command to navigate to main menu

--- MD3Desk/ViewModels/Monitor/MonitorViewModel.cs
+++ MD3Desk/ViewModels/Monitor/MonitorViewModel.cs
@@ -1,3 +1,5 @@
+using System.Windows.Input;
+using MD3Desk.Infrastructure.Commands.Base;
 using MD3Desk.ViewModels.Base;
 
 namespace MD3Desk.ViewModels.Monitor;
@@ -5,6 +7,23 @@ namespace MD3Desk.ViewModels.Monitor;
 public class MonitorViewModel : ViewModel
 {
     private readonly MainViewModel _parent;
+
+    #region Commands
+
+    #region NavigateMainMenuCommand
+        
+    private ICommand _navigateMainMenuCommand;
+    public ICommand NavigateMainMenuCommand => _navigateMainMenuCommand
+        ??= new RelayCommand(OnNavigateMainMenuCommandExecuted, CanNavigateMainMenuCommandExecute);
+
+    private void OnNavigateMainMenuCommandExecuted(object parameter)
+        => _parent.CurrentViewModel = _parent.MainMenuVm;
+        
+    private bool CanNavigateMainMenuCommandExecute(object parameter) => true;
+        
+    #endregion
+
+    #endregion
     
     public MonitorViewModel(MainViewModel parent)
     {
