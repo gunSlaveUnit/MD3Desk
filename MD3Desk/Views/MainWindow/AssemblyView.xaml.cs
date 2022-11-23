b5627ea2ae7c41daaccaac4df57a745a629b03fa
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-23 09:09:40 +0300

    Created empty assembly view with stack to map videos

---
+++ MD3Desk/Views/MainWindow/AssemblyView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MD3Desk.Views.MainWindow;
+
+public partial class AssemblyView : UserControl
+{
+    public AssemblyView()
+    {
+        InitializeComponent();
+    }
+}
