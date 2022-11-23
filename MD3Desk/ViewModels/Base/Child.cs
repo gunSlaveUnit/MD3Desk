363b9e28f0510fdfc7fef7af619006f77e6e599a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-23 09:00:49 +0300

    Child class is a template now to set parent type

--- MD3Desk/ViewModels/Base/Child.cs
+++ MD3Desk/ViewModels/Base/Child.cs
@@ -1,14 +1,14 @@
 namespace MD3Desk.ViewModels.Base;
 
-public class Child : ViewModel
+public class Child<T> : ViewModel
 {
     #region Properties
 
     #region Parent 
     
-    private StationEmulatorViewModel _parent;
+    private T _parent;
     
-    public StationEmulatorViewModel Parent
+    public T Parent
     {
         get => _parent;
         set => Set(ref _parent, value);
