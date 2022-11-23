9b60dca99762351b3c22991a3a2604b6fcfaf25f
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-23 10:52:30 +0300

    Added property to contain video names

--- MD3Desk/ViewModels/MainWindow/AssemblyViewModel.cs
+++ MD3Desk/ViewModels/MainWindow/AssemblyViewModel.cs
@@ -1,11 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using MD3Desk.ViewModels.Base;
 
 namespace MD3Desk.ViewModels.MainWindow;
 
 public class AssemblyViewModel : Child<MainViewModel>
 {
+    #region Properties
+
+    private const String VideosPath = "MD3Desk/Resources/Videos";
+
+    #region Videos
+    
+    #region Videos
+    
+    private List<String> _videos;
+    
+    public List<String> Videos
+    {
+        get => _videos;
+        private set => Set(ref _videos, value);
+    }
+    
+    #endregion
+
+    #endregion
+
+    #endregion
+    
     public AssemblyViewModel (MainViewModel parent)
     {
         Parent = parent;
+
+        Videos = new List<string>
+        {
+            Path.Combine(VideosPath, "1.wmv"),
+            Path.Combine(VideosPath, "2.wmv"),
+            Path.Combine(VideosPath, "3.wmv"),
+            Path.Combine(VideosPath, "4.wmv"),
+        };
     }
 }
