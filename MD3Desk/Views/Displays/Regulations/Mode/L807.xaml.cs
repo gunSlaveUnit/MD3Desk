0786d4d1080061afa6552f6d6d4f2fb06e011dfa
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-12-08 11:55:42 +0300

    Added L807 page

---
+++ MD3Desk/Views/Displays/Regulations/Mode/L807.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MD3Desk.Views.Displays.Regulations.Mode;
+
+public partial class L807 : UserControl
+{
+    public L807()
+    {
+        InitializeComponent();
+    }
+}
