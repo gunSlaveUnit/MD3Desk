b5d157d84e377010d21bceb52bb8a566db5b5cba
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-12-08 11:54:36 +0300

    Added regulations view

---
+++ MD3Desk/Views/Displays/Regulations/Regulations.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MD3Desk.Views.Displays.Regulations;
+
+public partial class Regulations : UserControl
+{
+    public Regulations()
+    {
+        InitializeComponent();
+    }
+}
