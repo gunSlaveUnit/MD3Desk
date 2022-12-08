ac7bbe8264db8de67cd7fd89a9d313836766b679
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-12-08 11:55:29 +0300

    Added mode page

---
+++ MD3Desk/Views/Displays/Regulations/Mode/Mode.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MD3Desk.Views.Displays.Regulations.Mode;
+
+public partial class Mode : UserControl
+{
+    public Mode()
+    {
+        InitializeComponent();
+    }
+}
